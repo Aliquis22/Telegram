227016be6289edd39911c35653a0b0bf5a292953
Aliquis22 <108261563+Aliquis22@users.noreply.github.com>
2023-02-21 10:42:20 +0700

    Куча строк с условным операторами, отвечающими за отображения дня недели,оптимизированы в словарь.

--- handlers/student.py
+++ handlers/student.py
@@ -4,21 +4,9 @@ from datetime import datetime, date, time
 from create_bot import dp, bot
 import os, json, datetime
 date =  str(date.today())
-day = datetime.datetime.today().weekday()
-if day == 0:
-    day="Понедельник"
-elif day == 1:
-    day="Вторник"
-elif day == 2:
-    day="Среда"
-elif day == 3:
-    day="Четверг"
-elif day == 4:
-    day="Пятница"
-elif day == 5:
-    day="Суббота"
-else:
-    day="Воскресение"
+days = {0:"Понедельник",1:"Вторник",2:"Среда",3:"Четверг",4:"Пятница",5:"Суббота",6:"Воскресение"}
+day = days[datetime.datetime.today().weekday()]
+
 with open("rasp.json", encoding = "utf8") as file:
     data = json.load(file)
 keys = list(data.keys())
@@ -71,35 +59,6 @@ async def button_reaction1(message: types.Message):
         await bot.send_message(message.from_user.id, "Сегодня занятий нет", parse_mode="Markdown",
                                reply_markup=kb_student)
 
-
-# async def button_reaction2(call: types.callback_query):
-#     s = "Расписание на эту неделю:" +"\n"
-#     s+=day + " " + date + "\n"
-#     await bot.answer_callback_query(call.id)
-#     for i in range(0,len(data)-1):
-#         if date == data[str(i)]['start'][:10]:
-#             start = data[str(i)]['start'][11:16]
-#             end  = data[str(i)]['end'][11:16]
-#             disciplina = data[str(i)]['disciplina'][0]['full_name']
-#             if len(data[str(i)]['lichnost']) != 0 :
-#                 teacher = data[str(i)]['lichnost'][0]['title']
-#             else:
-#                 teacher = ""
-#             if data[str(i)]['place'] != None:
-#                 corp  = "к." + data["0"]['place']['korpus']
-#                 aud = "ауд." + data["0"]['place']['nomer']
-#             else:
-#                 corp  = ""
-#                 aud = ""
-#             if ((i!=0) and disciplina != data[str(i-1)]['disciplina'][0]['full_name'])  :
-#                 s+=str(disciplina + "\n")
-#                 s+=str(start + " - " + end +"\n")
-#                 s+=str(teacher + "\n")
-#                 s += str(corp + " " + aud + "\n")
-#             else:
-#                  s+=str(teacher + "\n")
-#                  s += str(corp + " " + aud + "\n")
-#     await bot.send_message(call.message.chat.id, "Функция в разработке")
 def register_handlers_student(dp : Dispatcher):
     dp.register_message_handler(command_start,commands=['start', 'help'])
     dp.register_message_handler(button_reaction1, commands=['rasp'])
